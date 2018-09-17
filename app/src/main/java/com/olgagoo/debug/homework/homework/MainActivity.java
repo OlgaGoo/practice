8d5d079fc47ab868c7633558a0e80867fd864954
Olga Gurbo <olgagurbo.w@gmail.com>
2018-09-17 23:02:01 +0300

    Method for inverse text is added. Styles were added

--- app/src/main/java/com/olgagoo/debug/homework/homework/MainActivity.java
+++ app/src/main/java/com/olgagoo/debug/homework/homework/MainActivity.java
@@ -24,26 +24,36 @@ public class MainActivity extends AppCompatActivity {
         final TextView textViewOne = (TextView) findViewById(R.id.textView);
         final TextView textViewTwo = (TextView) findViewById(R.id.textView2);
 
+
         inverse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String current = textViewOne.getText().toString();
-                String one = (String) getText(R.string.text_view_one);
-                if(textViewOne.getText().toString().equals(one)){
-                    textViewOne.setText(R.string.text_view_two);
-                    textViewOne.setBackgroundColor(getResources().getColor(R.color.selected_text_background));
-                    textViewTwo.setText(R.string.text_view_one);
-                    textViewTwo.setBackgroundColor(getResources().getColor(R.color.unselected_text_background));
+                inverseText(textViewOne,textViewTwo);
 
-                }else {
-                    textViewOne.setText(R.string.text_view_one);
-                    textViewOne.setBackgroundColor(getResources().getColor(R.color.unselected_text_background));
-                    textViewTwo.setText(R.string.text_view_two);
-                    textViewTwo.setBackgroundColor(getResources().getColor(R.color.selected_text_background));
-                }
             }
+
         });
 
     }
+    
+
+    private void inverseText(TextView textViewOne, TextView textViewTwo) {
+        String current = textViewOne.getText().toString();
+        String one = (String) getText(R.string.text_view_one);
+        if(textViewOne.getText().toString().equals(one)){
+            textViewOne.setText(R.string.text_view_two);
+            textViewOne.setBackgroundColor(getResources().getColor(R.color.selected_text_background));
+            textViewTwo.setText(R.string.text_view_one);
+            textViewTwo.setBackgroundColor(getResources().getColor(R.color.unselected_text_background));
+
+        }else {
+            textViewOne.setText(R.string.text_view_one);
+            textViewOne.setBackgroundColor(getResources().getColor(R.color.unselected_text_background));
+            textViewTwo.setText(R.string.text_view_two);
+            textViewTwo.setBackgroundColor(getResources().getColor(R.color.selected_text_background));
+        }
+
+    }
+
 
 }
