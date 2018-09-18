4d2ae408bd6717ad4aa7ea453f65ea984e042fca
Olga Gurbo <olgagurbo.w@gmail.com>
2018-09-18 17:14:30 +0300

    3 options of OnClickListener implementation in mainActivity

--- app/src/main/java/com/olgagoo/debug/homework/homework/MainActivity.java
+++ app/src/main/java/com/olgagoo/debug/homework/homework/MainActivity.java
@@ -13,30 +13,80 @@ import com.olgagoo.debug.homework.R;
 
 import static com.olgagoo.debug.homework.R.color.unselected_text_background;
 
-public class MainActivity extends AppCompatActivity {
+public
+class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button inverse;
+    TextView textViewOne;
+    TextView textViewTwo;
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        inverse = (Button) findViewById(R.id.inverse_button);
+//        textViewOne = (TextView) findViewById(R.id.textView);
+//        textViewTwo = (TextView) findViewById(R.id.textView2);
+//
+//        //OPTION ONE - Anonymous
+//        inverse.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                inverseText(textViewOne,textViewTwo);
+//            }
+//        });
+//
+//        textViewOne.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                inverseText(textViewOne,textViewTwo);
+//            }
+//        });
+//
+//        textViewTwo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                inverseText(textViewOne,textViewTwo);
+//            }
+//        });
+//
+//        //OPTION TWO - with variable
+//        View.OnClickListener inverseClc = new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                inverseText(textViewOne,textViewTwo);
+//            }
+//        };
+//        inverse.setOnClickListener(inverseClc);
+//        textViewTwo.setOnClickListener(inverseClc);
+//        textViewOne.setOnClickListener(inverseClc);
+//
+//    }
+
+    //OPTION THREE - Implementation
     @Override
+    public void onClick(View v) {
+        inverseText(textViewOne,textViewTwo);
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button inverse = (Button) findViewById(R.id.inverse_button);
-        final TextView textViewOne = (TextView) findViewById(R.id.textView);
-        final TextView textViewTwo = (TextView) findViewById(R.id.textView2);
+        inverse = (Button) findViewById(R.id.inverse_button);
+        textViewOne = (TextView) findViewById(R.id.textView);
+        textViewTwo = (TextView) findViewById(R.id.textView2);
 
-
-        inverse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inverseText(textViewOne,textViewTwo);
-
-            }
-
-        });
-
+        inverse.setOnClickListener(this);
+        textViewOne.setOnClickListener(this);
+        textViewTwo.setOnClickListener(this);
     }
-    
 
+    /**
+     * Inverse Text and TextBackground between TextView One and TextView Two
+     * @param textViewOne is first textView
+     * @param textViewTwo is second textView
+     */
     private void inverseText(TextView textViewOne, TextView textViewTwo) {
         String current = textViewOne.getText().toString();
         String one = (String) getText(R.string.text_view_one);
@@ -54,6 +104,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
 
 }
